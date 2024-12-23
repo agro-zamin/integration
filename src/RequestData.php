42cf264d35c3dbd5e4745e66c68ee3befeb0b26d
uzdevid <uzdevid@gmail.com>
2024-12-23 19:36:21 +0500

    Добавил метод для сбора данных для логирования

--- src/RequestData.php
+++ src/RequestData.php
@@ -16,4 +16,48 @@ class RequestData implements ArrayableInterface {
     public array $options;
     public ResponseInterface|null $response = null;
     public Throwable $exception;
+
+    /**
+     * @return array
+     */
+    public function logData(): array {
+        $log['serviceName'] = $this->serviceName;
+
+        $requestBody = $this->requestModel->body();
+
+        if ($requestBody instanceof Payload) {
+            $requestBody = $requestBody->toArray();
+        }
+
+        $log['request'] = [
+            'id' => $this->requestId,
+            'url' => $this->requestModel->url(),
+            'method' => $this->requestModel->requestMethod(),
+            'headers' => $this->requestModel->headers(),
+            'queryPath' => $this->requestModel->queryPath(),
+            'queryParams' => $this->requestModel->queryParams(),
+            'option' => $this->requestModel->requestOption(),
+            'body' => $requestBody
+        ];
+
+        if (!is_null($this->response)) {
+            $log['response'] = [
+                'status' => $this->response->getStatusCode(),
+                'headers' => $this->response->getHeaders(),
+                'body' => $this->response->getBody()->getContents(),
+            ];
+        }
+
+        if (isset($this->exception)) {
+            $log['exception'] = [
+                'class' => $this->exception::class,
+                'message' => $this->exception->getMessage(),
+                'code' => $this->exception->getCode(),
+                'file' => $this->exception->getFile(),
+                'line' => $this->exception->getLine(),
+            ];
+        }
+
+        return $log;
+    }
 }
